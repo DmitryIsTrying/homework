c0a9791e2cf9495238be1a687fbd62027129d499
Dmitry <d2013.dima@mail.ru>
2024-09-28 19:12:50 +0500

    New homework, chat

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './s1-main/App'
+import reportWebVitals from './reportWebVitals'
+import store from './s2-homeworks/hw10/bll/store'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(
-    <App/>, document.getElementById('root')
-);
-// Что надо написать вместо ххх, чтобы в консоль вывело true?
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    <React.StrictMode>
+        {/*для дз 10*/}
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
