51c22ff4a9f37220cec387f7d6fc3ef4df4a3d44
Dmitry <d2013.dima@mail.ru>
2024-12-18 00:38:09 +0500

    hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,46 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
-
+import { hexToRgb, Slider, SliderProps, styled } from '@mui/material'
+const ThumbSlider = styled(Slider)(({ theme }) => ({
+  color: '#01CB22',
+  height: 3,
+  maxWidth: '147px',
+  padding: '13px 0',
+  '& .MuiSlider-thumb': {
+    height: 17,
+    width: 17,
+    backgroundColor: '#fff',
+    border: '1px solid currentColor',
+    '&:hover': {
+      boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
+    },
+    '&:before': {
+      content: '""', // Обязательно указать content
+      width: 6,
+      height: 6,
+      borderRadius: '50%',
+      backgroundColor: '#01CB22', // Замените на нужный цвет
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      boxShadow: 'none',
+    },
+  },
+  '& .MuiSlider-track': {
+    height: 4,
+  },
+  '& .MuiSlider-rail': {
+    color: '#8B8B8B',
+    opacity: 1,
+    height: 4,
+  },
+}))
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  return (
+    <ThumbSlider
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  )
 }
 
 export default SuperRange
